bff406b1c1241b3ae3f3b9d82123e8a1a6ddaf9f
megamind <megamindbrian@gmail.com>
2021-05-12 18:04:16 -0700

    working mmaze generator, still needs spawn weapons and lights

--- code/server/sv_bsp.c
+++ code/server/sv_bsp.c
@@ -397,6 +397,9 @@ static int SV_MakeHypercube( void ) {
 	strcpy(&output[offset], "}\n");
 	offset += 2;
 
+	vs[0][0] = vs[0][1] = vs[0][2] = -2000;
+	vs[1][0] = vs[1][1] = vs[1][2] = 2000;
+
 	strcpy(&output[offset], 
 		va("{\n"
 		"\"classname\" \"misc_skybox\"\n"
@@ -746,12 +749,13 @@ static int SV_MakeMaze( void ) {
 	gridRows |= 1; // make odd or +1 so theres a wall on both sides
 	gridCols |= 1;
 	int wallWidth = 16;
-	int totalWidth = cellWidth * gridCols + wallWidth * (gridCols - 1);
-	int totalHeight = cellHeight * gridRows + wallWidth * (gridRows - 1);
+	int totalWidth = cellWidth * (gridCols / 2) + wallWidth * ((gridCols / 2) - 1);
+	int totalHeight = cellHeight * (gridRows / 2) + wallWidth * ((gridRows / 2) - 1);
 	vec3_t  vs[2];
 	// layout the maze just for debugging
 	char maze[gridCols][gridRows];
 	memset(maze, ' ', sizeof(maze));
+	/*
 	for(int x = 0; x < gridCols; x++) {
 		for(int y = 0; y < gridRows; y++) {
 			maze[0][y] = '#';
@@ -760,6 +764,7 @@ static int SV_MakeMaze( void ) {
 			maze[x][gridRows-1] = '#';
 		}
 	}
+	*/
 	int *areaStack = Hunk_AllocateTempMemory(gridRows * gridCols * 4 * sizeof(int));
 	int stackI = 0;
 	int minX;
@@ -789,10 +794,24 @@ static int SV_MakeMaze( void ) {
 	SV_SetStroke("sky1");
 	strcpy(&output[offset], SV_MakeBox(vs[0], vs[1]));
 	offset += strlen(&output[offset]);
+	
+	// make offsets for centering
+	vs[0][0] = -(totalWidth / 2);
+	vs[1][0] = +(totalWidth / 2);
+
+	vs[0][1] = -(totalHeight / 2);
+	vs[1][1] = +(totalHeight / 2);
+
+	vs[0][2] = -(cellWidth / 2);
+	vs[1][2] = (cellHeight / 2);
+	
+	SV_SetStroke(va("cube%i", 0));
+	strcpy(&output[offset], SV_MakeBox(vs[0], vs[1]));
+	offset += strlen(&output[offset]);
 
 	// interesting, I could make this non-recursive by scanning the maze for spaces
 	//   to divide basically using the character grid as it's own virtual call stack
-	while(safety < (gridRows / 2) * (gridCols / 2)) {
+	while(safety < 6) {
 		// initialize the spaces with the entire maze
 		stackI--;
 		if(stackI == -1) {
@@ -810,12 +829,12 @@ static int SV_MakeMaze( void ) {
 			maxX = areaStack[stackI*4+2];
 			maxY = areaStack[stackI*4+3];
 		}
-		//Com_Printf("Maze block: %i x %i <> %i x %i\n", minX, minY, maxX, maxY);
+		Com_Printf("Maze block: %i x %i <> %i x %i\n", minX, minY, maxX, maxY);
 		int whichDirection = rand() % 4;
 		int wallX = (rand() % (maxX - minX)) + minX;
 		int wallY = (rand() % (maxY - minY)) + minY;
 
-		//Com_Printf("Maze walls: %i x %i\n", wallX, wallY);
+		Com_Printf("Maze walls: %i x %i\n", wallX, wallY);
 		// add the 4 walls to the stack for sub-dividing
 		if(wallX - minX > 1
 			&& wallY - minY > 1) {
@@ -830,7 +849,7 @@ static int SV_MakeMaze( void ) {
 			&& maxY - wallY > 1) {
 			//Com_Printf("Adding bottom, left: %i x %i <> %i x %i\n", minX, wallY, wallX, maxY);
 			areaStack[stackI*4+0] = minX;
-			areaStack[stackI*4+1] = wallY;
+			areaStack[stackI*4+1] = wallY + 1;
 			areaStack[stackI*4+2] = wallX;
 			areaStack[stackI*4+3] = maxY;
 			stackI++;
@@ -838,7 +857,7 @@ static int SV_MakeMaze( void ) {
 		if(maxX - wallX > 1
 			&& wallY - minY > 1) {
 			//Com_Printf("Adding top, right: %i x %i <> %i x %i\n", wallX, minY, maxX, wallY);
-			areaStack[stackI*4+0] = wallX;
+			areaStack[stackI*4+0] = wallX + 1;
 			areaStack[stackI*4+1] = minY;
 			areaStack[stackI*4+2] = maxX;
 			areaStack[stackI*4+3] = wallY;
@@ -847,22 +866,12 @@ static int SV_MakeMaze( void ) {
 		if(maxX - wallX > 1
 			&& maxY - wallY > 1) {
 			//Com_Printf("Adding bottom, right: %i x %i <> %i x %i\n", wallX, wallY, maxX, maxY);
-			areaStack[stackI*4+0] = wallX;
-			areaStack[stackI*4+1] = wallY;
+			areaStack[stackI*4+0] = wallX + 1;
+			areaStack[stackI*4+1] = wallY + 1;
 			areaStack[stackI*4+2] = maxX;
 			areaStack[stackI*4+3] = maxY;
 			stackI++;
 		}
-		
-		// make offsets for this cell
-		vs[0][0] = -(totalWidth / 2) + (minX * (cellWidth + wallWidth));
-		vs[1][0] = -(totalWidth / 2) + (maxX * (cellWidth + wallWidth)) + cellWidth;
-
-		vs[0][1] = -(totalHeight / 2) + (minY * (cellHeight + wallWidth));
-		vs[1][1] = -(totalHeight / 2) + (maxY * (cellHeight + wallWidth)) + cellHeight;
-
-		vs[0][2] = -(cellWidth / 2);
-		vs[1][2] = (cellHeight / 2);
 
 		for(int i = 0; i < 3; i++) {
 			// make 4 or 5 walls around 3 gaps dividing 4 spaces, 
@@ -882,10 +891,61 @@ static int SV_MakeMaze( void ) {
 				if(i < 2) { // already place 2 walls must be on the other side
 					if(wallX - minX <= 1) gap = wallX - 1;
 					else gap = ((rand() % (wallX - minX)) + minX);
+
+					int wall[2][3] = {
+						{vs[0][0] + (minX - 1) * (cellWidth + wallWidth),    
+						 vs[0][1] + wallY * (cellHeight + wallWidth), 
+						 vs[0][2]},
+
+						{vs[0][0] + gap * (cellWidth + wallWidth) + wallWidth,
+						 vs[0][1] + wallY * (cellHeight + wallWidth) + wallWidth,
+						 vs[1][2]}
+					};
+					strcpy(&output[offset], SV_MakeWall(wall[0], wall[1]));
+					offset += strlen(&output[offset]);
+					int wall2[2][3] = {
+						{vs[0][0] + (gap + 1) * (cellWidth + wallWidth) - wallWidth,    
+						 vs[0][1] + wallY * (cellHeight + wallWidth), 
+						 vs[0][2]},
+
+						{vs[0][0] + (i == 1 
+							? maxX * (cellWidth + wallWidth)
+							: wallX * (cellWidth + wallWidth)),
+						 vs[0][1] + wallY * (cellHeight + wallWidth) + wallWidth,
+						 vs[1][2]}
+					};
+					strcpy(&output[offset], SV_MakeWall(wall2[0], wall2[1]));
+					offset += strlen(&output[offset]);
 				} else {
 					if(maxX - wallX <= 1) gap = wallX;
 					else gap = ((rand() % (maxX - wallX)) + wallX);
+
+					int wall[2][3] = {
+						{vs[0][0] + (i == 1
+							? (minX - 1) * (cellWidth + wallWidth)
+							: wallX * (cellWidth + wallWidth)),    
+						 vs[0][1] + wallY * (cellHeight + wallWidth), 
+						 vs[0][2]},
+
+						{vs[0][0] + gap * (cellWidth + wallWidth) + wallWidth,
+						 vs[0][1] + wallY * (cellHeight + wallWidth) + wallWidth,
+						 vs[1][2]}
+					};
+					strcpy(&output[offset], SV_MakeWall(wall[0], wall[1]));
+					offset += strlen(&output[offset]);
+					int wall2[2][3] = {
+						{vs[0][0] + (gap + 1) * (cellWidth + wallWidth) - wallWidth,    
+						 vs[0][1] + wallY * (cellHeight + wallWidth), 
+						 vs[0][2]},
+
+						{vs[0][0] + maxX * (cellWidth + wallWidth),
+						 vs[0][1] + wallY * (cellHeight + wallWidth) + wallWidth,
+						 vs[1][2]}
+					};
+					strcpy(&output[offset], SV_MakeWall(wall2[0], wall2[1]));
+					offset += strlen(&output[offset]);
 				}
+//Com_Printf("Maze door: %i x %i\n", gap * 2 + 1, wallY * 2);
 				for(int fillX = (minX - 1) * 2; fillX <= maxX * 2; fillX++) {
 					if(maze[fillX][wallY*2] == '*') 
 						continue;
@@ -900,10 +960,61 @@ static int SV_MakeMaze( void ) {
 				if(i < 2) {
 					if(wallY - minY <= 1) gap = wallY - 1;
 					else gap = ((rand() % (wallY - minY)) + minY);
+
+					int wall[2][3] = {
+						{vs[0][0] + wallX * (cellWidth + wallWidth),    
+						 vs[0][1] + (minY - 1) * (cellHeight + wallWidth), 
+						 vs[0][2]},
+
+						{vs[0][0] + wallX * (cellWidth + wallWidth) + wallWidth,
+						 vs[0][1] + gap * (cellHeight + wallWidth) + wallWidth,
+						 vs[1][2]}
+					};
+					strcpy(&output[offset], SV_MakeWall(wall[0], wall[1]));
+					offset += strlen(&output[offset]);
+					int wall2[2][3] = {
+						{vs[0][0] + wallX * (cellWidth + wallWidth),    
+						 vs[0][1] + (gap + 1) * (cellHeight + wallWidth) - wallWidth, 
+						 vs[0][2]},
+
+						{vs[0][0] + wallX * (cellWidth + wallWidth) + wallWidth,
+						 vs[0][1] + (i == 1 
+						 	? maxY * (cellHeight + wallWidth)
+						  : wallY * (cellHeight + wallWidth)),
+						 vs[1][2]}
+					};
+					strcpy(&output[offset], SV_MakeWall(wall2[0], wall2[1]));
+					offset += strlen(&output[offset]);
 				} else {
 					if(maxY - wallY <= 1) gap = wallY;
 					else gap = ((rand() % (maxY - wallY)) + wallY);
+
+					int wall[2][3] = {
+						{vs[0][0] + wallX * (cellWidth + wallWidth),    
+						 vs[0][1] + (i == 1
+						 ? (minY - 1) * (cellHeight + wallWidth)
+						 : wallY * (cellHeight + wallWidth)), 
+						 vs[0][2]},
+
+						{vs[0][0] + wallX * (cellWidth + wallWidth) + wallWidth,
+						 vs[0][1] + gap * (cellHeight + wallWidth) + wallWidth,
+						 vs[1][2]}
+					};
+					strcpy(&output[offset], SV_MakeWall(wall[0], wall[1]));
+					offset += strlen(&output[offset]);
+					int wall2[2][3] = {
+						{vs[0][0] + wallX * (cellWidth + wallWidth),    
+						 vs[0][1] + (gap + 1) * (cellHeight + wallWidth) - wallWidth, 
+						 vs[0][2]},
+
+						{vs[0][0] + wallX * (cellWidth + wallWidth) + wallWidth,
+						 vs[0][1] + maxY * (cellHeight + wallWidth),
+						 vs[1][2]}
+					};
+					strcpy(&output[offset], SV_MakeWall(wall2[0], wall2[1]));
+					offset += strlen(&output[offset]);
 				}
+//Com_Printf("Maze door: %i x %i\n", wallX * 2, gap * 2 + 1);
 				for(int fillY = (minY - 1) * 2; fillY <= maxY * 2; fillY++) {
 					if(maze[wallX*2][fillY] == '*') 
 						continue;
@@ -924,19 +1035,6 @@ static int SV_MakeMaze( void ) {
 					 vs[1][2]}
 				};
 */
-int wall[2][3] = {
-	{wallX * (cellWidth + wallWidth),    
-	 (minY - 1) * (cellHeight + wallWidth), 
-	 0},
-
-	{wallX * (cellWidth + wallWidth) + wallWidth,
-	 gap * (cellHeight + wallWidth),
-	 cellHeight}
-};
-Com_Printf("Drawing wall: %i x %i <> %i x %i\n", wall[0][0], wall[0][1],
-	wall[1][0], wall[1][1]);
-				strcpy(&output[offset], SV_MakeWall(wall[0], wall[1]));
-				offset += strlen(&output[offset]);
 
 			}
 		}
@@ -955,6 +1053,9 @@ Com_Printf("Drawing wall: %i x %i <> %i x %i\n", wall[0][0], wall[0][1],
 
 	strcpy(&output[offset], "}\n");
 	offset += 2;
+
+	vs[0][0] = vs[0][1] = vs[0][2] = -2000;
+	vs[1][0] = vs[1][1] = vs[1][2] = 2000;
 
 	strcpy(&output[offset], 
 		va("{\n"
